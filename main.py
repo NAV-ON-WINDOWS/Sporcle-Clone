33cc0d112708f80acb4f6adf30c83e17cb5604d2
NAV-ON-WINDOWS <arnav.salgarkar@gmail.com>
2026-04-09 23:49:41 +0530

    made the screen working, got coordinates for all the capitals, put them all into an excel file and converted to .csv, added that .csv file

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
-# main.py test
 import turtle as t
+import pandas as pd
 
 screen = t.Screen()
+screen.setup(width=982, height=800)
 screen.title("Indian States Guessing Game")
 
-t.exitonclick()
+img = "india_map.gif"
+t.addshape(img)
+
+tim = t.Turtle()
+tim.shape(img)
+
+def get_mouse_click(x,y):
+    print(f"{x}, {y}")
+t.onscreenclick(get_mouse_click)
+
+data = pd.read_csv("capitals.csv")
+print(data)
+
+screen.exitonclick()
