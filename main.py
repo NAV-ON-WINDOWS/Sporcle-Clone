07b5cb7e7dbd709df0bc98fd041ce59051956b6f
NAV-ON-WINDOWS <arnav.salgarkar@gmail.com>
2026-04-10 00:22:54 +0530

    modified main.py (input handelling, printing answers and handeling discrepancies) & modified .csv

--- main.py
+++ main.py
@@ -11,11 +11,37 @@ t.addshape(img)
 tim = t.Turtle()
 tim.shape(img)
 
-def get_mouse_click(x,y):
-    print(f"{x}, {y}")
-t.onscreenclick(get_mouse_click)
+# def get_mouse_click(x,y):
+#     print(f"{x}, {y}")
+# t.onscreenclick(get_mouse_click)
 
 data = pd.read_csv("capitals.csv")
-print(data)
+# print(data)
 
-screen.exitonclick()
+guessed_capitals = []
+capitals = data["Capital"].to_list()
+
+while len(guessed_capitals) < len(capitals):
+    answer = screen.textinput(title=f"{len(guessed_capitals) / 27}",
+                              prompt="Enter your answer: ").title()
+
+    if answer == "Exit":
+        missing_capitals = []
+        for capital in capitals:
+            if capital not in guessed_capitals:
+                missing_capitals.append(capital)
+        print("Capitals you couldn't guess are:")
+        print(f" \n".join(missing_capitals))
+
+        break
+
+    if answer in capitals:
+        guessed_capitals.append(answer)
+        tpen = t.Turtle()
+        tpen.hideturtle()
+        tpen.penup()
+        capital_data = data[data.Capital == answer]
+        tpen.goto(capital_data["x"].item(), capital_data["y"].item())
+        tpen.write(answer)
+
+screen.mainloop()
